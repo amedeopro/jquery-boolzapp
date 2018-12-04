6b821d28542fae3d4544d83d0f9de52aa11bd6c2
amedeopro <amedeopro@me.com>
2018-12-04 23:12:50 +0100

    clear is not defined

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('#newMessage').keypress(function(event){
+    if (event.which == 13) {
+      var nuovoMessaggio = $('#newMessage').val();
+
+      var nuovoTemplateMessaggio = $('.templateMessage .new-message');
+
+      var clearTemplate = $('.templateMessage .clear');
+
+      nuovoTemplateMessaggio.text(nuovoMessaggio);
+
+      $('.all-message-container').append(nuovoTemplateMessaggio);
+      $('.all-message-container').append(clear);
+
+      $('#newMessage').val('');
+    }
+  })
+});
